3325d48ddc7b6741a7a0306a07ec64a62bd03740
LeoGrau <leonardo.grau@outlook.com.pe>
2024-08-21 04:42:04 -0500

    feat: navbar first commit

---
+++ src/orlando/components/carousel/circular-list.ts
@@ -0,0 +1,43 @@
+// Nodo Circular
+class RoundedNode<T> {
+  next: RoundedNode<T> | null = null;
+  constructor(public value: T) {}
+}
+
+class RoundedList<T> {
+  head: RoundedNode<T> | null = null
+
+  add(value: T) {
+    const _add = (value: T, currentNode: RoundedNode<T>) => {
+      console.log("why", currentNode)
+      if(currentNode.next === null) {
+        currentNode.next = new RoundedNode<T>(value)
+      } else {
+        _add(value, currentNode.next!)
+      }
+    }
+
+    if(!this.head)
+      this.head = new RoundedNode<T>(value)
+    else
+      _add(value, this.head!)
+  }
+  push(value: T) {
+    const _add = (value: T, currentNode: RoundedNode<T>) => {
+      console.log("why", currentNode)
+      if(currentNode === null) {
+        currentNode = new RoundedNode<T>(value)
+      } else {
+        console.log("ass",currentNode.next!)
+        _add(value, currentNode.next!)
+      }
+    }
+
+    if(!this.head)
+      this.head = new RoundedNode<T>(value)
+    else
+      _add(value, this.head!)
+  }
+}
+ 
+export default RoundedList
--- src/orlando/components/carousel/main-carousel-or.vue
+++ src/orlando/components/carousel/main-carousel-or.vue
@@ -1,9 +1,22 @@
-<template >
-    <div>
-        
+<template>
+    <div class="main-carousel-bar">
+    
     </div>
 </template>
-<script setup>
+<script lang="ts" setup>
+import RoundedList from './circular-list';
+import { ref } from 'vue';
+const imageCarousel = ref(new RoundedList<number>())
+
+function setImages() {
+    console.log(imageCarousel.value)
+    imageCarousel.value.push(9)
+    imageCarousel.value.push(12)
+    imageCarousel.value.push(19)
+    console.log(imageCarousel.value)
+}
+
+setImages()
 
 </script>
 <style >
