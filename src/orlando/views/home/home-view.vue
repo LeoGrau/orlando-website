f3127c6ceedd2732193855ec975649645560c944
Fabrizio David Felix Barra Castañeda <fabriziobc2020@outlook.com>
2024-08-24 19:09:18 -0500

    Feat : add 7 favorites dishes

--- src/orlando/views/home/home-view.vue
+++ src/orlando/views/home/home-view.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <main-carousel-or></main-carousel-or>
+    <menu-card-or></menu-card-or>
     <footer-or></footer-or>
   </div>
 </template>
 <script setup lang="ts">
 import MainCarouselOr from '@/orlando/components/carousel/main-carousel-or.vue';
 import FooterOr from '@/orlando/components/footer/footer-or.vue';
+import menuCardOr from '@/orlando/components/menucard/menu-card-or.vue';
 </script>
 <style lang="">
   
