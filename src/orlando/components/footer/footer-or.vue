4fc92339671ed403d158a6ff0b3545db16d17fb3
LeoGrau <leonardo.grau@outlook.com.pe>
2024-08-25 01:32:21 -0500

    feat: navbar first commit

--- src/orlando/components/footer/footer-or.vue
+++ src/orlando/components/footer/footer-or.vue
@@ -1,36 +1,36 @@
 <template>
   <div
-    class="w-full bg-black h-[40vh] flex justify-center items-center flex-col"
+    class="w-full bg-black h-max flex justify-center items-center flex-col p-5"
   >
-    <div class="w-10/12 h-[30vh] flex flex-row justify-evenly">
-      <div class="w-1/4">
+    <div class="flex flex-row justify-evenly gap-2">
+      <div class="w-1/3 p-4">
         <div>
           <h1 class="text-xl underlinediv text-white">Nosotros</h1>
         </div>
         <div>
           <p class="text-white text-justify my-1">
-             En "Orlando", llevamos más de 50 años dedicados a ofrecer el mejor chicharrón de pollo
-              y pollo a la brasa en Nasca, acompañado de nuestra exclusiva Salsa 7
-              Sabores y el auténtico ají de Nasca. Con una tradición de sabor y
-              calidad, brindamos a nuestros clientes una experiencia única
-              en un ambiente familiar y acogedor.
+            En "Orlando", llevamos más de 50 años dedicados a ofrecer el mejor
+            chicharrón de pollo y pollo a la brasa en Nasca, acompañado de
+            nuestra exclusiva Salsa 7 Sabores y el auténtico ají de Nasca. Con
+            una tradición de sabor y calidad, brindamos a nuestros clientes una
+            experiencia única en un ambiente familiar y acogedor.
           </p>
         </div>
       </div>
-      <div class="w-1/4">
+      <div class="w-1/3 p-4">
         <div>
           <h1 class="text-xl underlinediv text-white">Servicios</h1>
         </div>
         <div>
           <p class="text-white text-justify my-1">
-            Ofrecemos un menú variado con nuestras especialidades en delicioso 
-            Pollo a la brasa 7 sabores y los crocantes chicharrones de pollo, disponibles para disfrutar en nuestros
-            locales o a través de nuestro servicio de entrega a domicilio.
-            
+            Ofrecemos un menú variado con nuestras especialidades en delicioso
+            Pollo a la brasa 7 sabores y los crocantes chicharrones de pollo,
+            disponibles para disfrutar en nuestros locales o a través de nuestro
+            servicio de entrega a domicilio.
           </p>
         </div>
       </div>
-      <div class="w-1/4">
+      <div class="w-1/3 p-4">
         <div>
           <h1 class="text-xl underlinediv text-white">Contáctanos</h1>
         </div>
@@ -41,38 +41,43 @@
             realizar pedidos o reservas, y seguirnos en nuestras redes sociales
             para estar al tanto de nuestras promociones y novedades.
           </p>
+          <div class="flex justify-end gap-2">
+            <a
+              href="https://www.facebook.com/PolleriaOrlando?locale=es_LA"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/733/733547.png"
+                alt="Facebook"
+                class="w-8 h-8"
+              />
+            </a>
+            <a
+              href="https://wa.me/51978800056"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/733/733585.png"
+                alt="WhatsApp"
+                class="w-8 h-8"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
-    <div class="w-full flex justify-evenly text-sm">
-  <div class="text-white ">
-    © 2024, Orlandos | Todos los derechos reservados
-  </div>
-  <div class="text-white">Esta pagina fue creada por Kipuh!</div>
-  <div class="flex w-1/12 justify-evenly">
-    <a
-      href="https://www.facebook.com/PolleriaOrlando?locale=es_LA"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" class="w-8 h-8" />
-    </a>
-    <a
-      href="https://wa.me/51978800056"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="WhatsApp" class="w-8 h-8" />
-    </a>
-  </div>
-</div>
-
+    <div class="w-full flex flex-col items-center text-sm">
+      <span class="text-white">Esta pagina fue creada por Kipuh!</span>
+      <span class="text-white">
+        © 2024, Orlandos | Todos los derechos reservados
+      </span>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .underlinediv {
--- src/orlando/components/menucard/menu-card-or.vue
+++ src/orlando/components/menucard/menu-card-or.vue
@@ -1,119 +1,119 @@
 <template>
-    <pv-tabs value="0">
-        <div class="flex w-full justify-center">
-            <pv-tab-list>
-                <pv-tab class="text-orange-500 text-lg" value="0">Los 7 Favoritos</pv-tab>
-                <pv-tab class="text-orange-500 text-lg" value="1">Carta</pv-tab>
-                <pv-tab class="text-orange-500 text-lg" value="2">Bebidas</pv-tab>
-            </pv-tab-list>
+<div class="h-max">
+  <pv-tabs value="0">
+    <div class="flex w-full justify-center">
+      <pv-tab-list>
+        <pv-tab class="text-orange-500 text-lg" value="0"
+          >Los 7 Favoritos</pv-tab
+        >
+        <pv-tab class="text-orange-500 text-lg" value="1">Carta</pv-tab>
+        <pv-tab class="text-orange-500 text-lg" value="2">Bebidas</pv-tab>
+      </pv-tab-list>
+    </div>
+    <pv-tab-panels>
+      <pv-tab-panel value="0">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          <div v-for="foodItem in foodItems"
+            class="flex items-center justify-evenly shadow-md  rounded-md p-3 border-zinc-100 border"
+          >
+            <img
+              class="w-1/3"
+              :src="foodItem.imageUrl"
+              alt="Mostrito"
+            />
+            <div class="flex flex-col justify-center items-center">
+              <span class="text-lg font-medium text-black">
+                {{ foodItem.name }}</span
+              >
+              <span
+                class="text-md flex items-center justify-center text-white font-medium px-5 py-2 bg-red-600 rounded-full"
+                > {{ currency.symbol + "" + foodItem.price }} </span
+              >
+            </div>
+          </div>
         </div>
-        <pv-tab-panels>
-            <pv-tab-panel value="0">
-                <div class="w-full flex justify-evenly my-3">
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40 h-max"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7"
-                            alt="Chicharron de Pollo">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> 1/4 Chicharron de Pollo</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/25</span>
-                        </div>
-
-
-                    </div>
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/337687435_875489670188295_2880514990359156885_n.png?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=XY3GoDIRKsQQ7kNvgGgi1GD&_nc_ht=scontent.fpio3-1.fna&oh=00_AYAjar5Fm8Gs0_UnjzkQgvbEIe18NdmyihH-NQ_LWhvuLA&oe=66D049F0"
-                            alt="1/4 de Pollo a la Brasa">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> 1/4 Pollo a la Brasa</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/27</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full flex justify-evenly my-3">
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/447193791_956585819495196_2052375901242093334_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=j0if0x63_ZkQ7kNvgFb8Yh9&_nc_ht=scontent.fpio3-1.fna&_nc_gid=A8KvkGQGmaBt-8vgabRaKG_&oh=00_AYBGzciQGyfFaPtsAf2VYkwXQyB-J2uqbGxbSa2ZwPW39A&oe=66D04D70"
-                            alt="Mostrito">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> Mostrito a la Brasa</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/27</span>
-                        </div>
-                    </div>
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/434487798_921349746352137_566529925219583146_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=ozuOgS8UNzsQ7kNvgH4iaNM&_nc_ht=scontent.fpio3-1.fna&oh=00_AYDBTl2p0UsojOQmbhIyssKm31V28askoD_lUMUKgzdO2A&oe=66D02E88"
-                            alt="Milanesa de Pollo">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> Milanesa de Pollo</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/30</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full flex justify-evenly my-3">
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/449958789_976410930846018_4192329271915356286_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=7eDBMqw3cJMQ7kNvgG6oGUN&_nc_ht=scontent.fpio3-1.fna&oh=00_AYAjxeL_vifKXjY4uCAmml4l_28aLFCBnKY86PIfXdmHIw&oe=66D01C47"
-                            alt="Tallarin Saltado">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> Tallarin Saltado</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/27</span>
-                        </div>
-                    </div>
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/454266618_995463712274073_8263001984218094649_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=sChUaFQwKTUQ7kNvgFpM8-t&_nc_ht=scontent.fpio3-1.fna&oh=00_AYAq1pfY9cUO3m3Na6TZ_4rmpY7yKnPx2Sgk6IOAe_40ZQ&oe=66D0330E"
-                            alt="Sopa Wantan">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> Sopa Wantan</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/20</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="w-full flex justify-evenly my-3">
-                    <div class="w-3/12 flex items-center justify-evenly h-40 shadow-lg rounded-md">
-                        <img class="w-40"
-                            src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/440404127_937535644733547_4952781826825298586_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=tEK5b3Siv_MQ7kNvgGP22m6&_nc_ht=scontent.fpio3-1.fna&oh=00_AYDhlAMXJs6eA0llGQ5bBqX1ATeiTv6f_mPjv0V4lKDQyg&oe=66D02F56"
-                            alt="Arroz Chaufa de Pollo">
-                        <div class="flex flex-col h-full justify-evenly items-center">
-                            <span class="text-lg font-medium text-black"> Arroz Chaufa de Pollo</span>
-                            <span
-                                class="text-lg flex items-center justify-center text-white font-medium h-12 w-12 bg-red-600 rounded-full">s/25</span>
-                        </div>
-                    </div>
-
-                </div>
-            </pv-tab-panel>
-            <pv-tab-panel value="1">
-                <p class="m-0">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                    totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo. Nemo enim
-                    ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-                    dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam
-                    eius modi.
-                </p>
-            </pv-tab-panel>
-            <pv-tab-panel value="2">
-                <p class="m-0">
-                    At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
-                    deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non
-                    provident, similique sunt in culpa
-                    qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
-                    est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-                    impedit quo minus.
-                </p>
-            </pv-tab-panel>
-        </pv-tab-panels>
-    </pv-tabs>
+      </pv-tab-panel>
+      <pv-tab-panel value="1">
+        <p class="m-0">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit,
+          sed quia non numquam eius modi.
+        </p>
+      </pv-tab-panel>
+      <pv-tab-panel value="2">
+        <p class="m-0">
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt in culpa qui officia deserunt mollitia animi, id est
+          laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+          distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+          cumque nihil impedit quo minus.
+        </p>
+      </pv-tab-panel>
+    </pv-tab-panels>
+  </pv-tabs>
+</div>
 </template>
 <script setup lang="ts">
+import { ref } from "vue";
+
+interface FoodItem {
+  imageUrl: string;
+  price: number;
+  name: string;
+}
+
+const availableCurrencies = ref([{ symbol: 'S/.', name: 'PEN'}, { symbol: '$', name: 'USD'}])
+const currency = ref(availableCurrencies.value[0])
+
+const foodItems = ref([
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+  {
+    imageUrl:
+      "https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/398027037_837183284768784_4333519193526618187_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=yjj95IYpYHEQ7kNvgH3Uz1w&_nc_ht=scontent.fpio3-1.fna&oh=00_AYCl8ftfycC1tiryZNyiMRkK1QHrOnYvcRk8WdvQrInj1Q&oe=66D02DD7",
+    name: "1/4 Chicharron de Pollo",
+    price: 24,
+  },
+] as Array<FoodItem>);
 </script>
-<style></style>
+<style scoped></style>
